dffc39ed9646f663dc3799598c1ddeb720477766
Esdras Lopez <5963060+EzraLopez@users.noreply.github.com>
2018-05-13 22:47:29 -0500

    Add more kotlin sweetness to FeedbackBottomDialogFragment

--- app/src/main/java/com/esdraslopez/android/nearbychat/login/FeedbackBottomDialogFragment.kt
+++ app/src/main/java/com/esdraslopez/android/nearbychat/login/FeedbackBottomDialogFragment.kt
@@ -1,10 +1,8 @@
 package com.esdraslopez.android.nearbychat.login
 
 import android.content.ActivityNotFoundException
-import android.content.Context
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.support.design.widget.BottomSheetDialogFragment
 import android.view.LayoutInflater
@@ -15,32 +13,18 @@ import com.esdraslopez.android.nearbychat.BuildConfig
 import com.esdraslopez.android.nearbychat.R
 import com.esdraslopez.android.nearbychat.Util
 import kotlinx.android.synthetic.main.layout_feedback_bottom_sheet.view.*
-import java.util.*
 
 class FeedbackBottomDialogFragment : BottomSheetDialogFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-
         val view = inflater.inflate(R.layout.layout_feedback_bottom_sheet, container,
                 false)
 
         view.play_store_review_button.setOnClickListener {
             val packageName = context?.packageName
-            val uri = Uri.parse("market://details?id=$packageName")
+            val playStoreIntent = Intent(Intent.ACTION_VIEW)
+            playStoreIntent.data = Uri.parse("market://details?id=$packageName")
 
-            val playStoreIntent = Intent(Intent.ACTION_VIEW, uri)
-
-            // to be taken back to our application after going the play store
-            // we need to add following flags to the intent
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                playStoreIntent.addFlags(
-                        Intent.FLAG_ACTIVITY_NO_HISTORY or
-                                Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
-                                Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-            } else {
-                playStoreIntent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY or
-                        Intent.FLAG_ACTIVITY_MULTIPLE_TASK)
-            }
             try {
                 startActivity(playStoreIntent)
             } catch (e: ActivityNotFoundException) {
@@ -49,7 +33,7 @@ class FeedbackBottomDialogFragment : BottomSheetDialogFragment() {
             }
         }
 
-        view.github_issue_button.setOnClickListener { Util.goToURL(Objects.requireNonNull<Context>(context), "https://github.com/EzraLopez/nearbychat/issues/new") }
+        view.github_issue_button.setOnClickListener { Util.goToURL(activity, "https://github.com/EzraLopez/nearbychat/issues/new") }
 
         view.contact_developer_button.setOnClickListener {
             val email = "androidapps@esdraslopez.com"
@@ -60,7 +44,6 @@ class FeedbackBottomDialogFragment : BottomSheetDialogFragment() {
         }
 
         return view
-
     }
 
     private fun composeEmail(email: String, subject: String, message: String) {
@@ -69,10 +52,10 @@ class FeedbackBottomDialogFragment : BottomSheetDialogFragment() {
                 .putExtra(Intent.EXTRA_SUBJECT, subject)
                 .putExtra(Intent.EXTRA_TEXT, message)
 
-        if (intent.resolveActivity(Objects.requireNonNull<Context>(context).getPackageManager()) != null)
+        if (intent.resolveActivity(context?.packageManager) != null)
             startActivity(intent)
         else
-            Toast.makeText(context, getString(R.string.email_app_not_found_error), Toast.LENGTH_SHORT).show()
+            Toast.makeText(activity, getString(R.string.email_app_not_found_error), Toast.LENGTH_SHORT).show()
     }
 
     companion object {
